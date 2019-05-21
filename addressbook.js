bbfc5f1a822307129fe2e42d54e106a497d603e2
Kiley Lewis <kileylewis32@gmail.com>
2019-05-21 17:29:13 -0500

    save

--- addressbook.js
+++ addressbook.js
@@ -3,15 +3,7 @@ const newUser = [];
 function get(){
     fetch("https://randomuser.me/api/")
       .then( res => res.json())
-      .then( data => {
-        newUser.push(data);
-        console.log(data);
-        const fname = newUser[i].results["0"].name.first;
-        firstname.innerHTML =
-            user name: ${fname}
-
-
-      })
+      .then( data => console.log("Array of data: ",data))
       .catch( err => console.log("oops, looks like we got an error: ", err))
       .finally( ()=> console.log("finally, This function always runs...")) // Whether or not there's an error or success, this will happen such as stopping a loading wheel on the front end
       
